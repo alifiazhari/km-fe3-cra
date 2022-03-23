183dc8508faabd2dd842f9e0fb549b69ef039ad5
Alif <alifia71001@gmail.com>
2022-03-23 00:49:56 -0700

    try again

---
+++ homework-module-2/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import "./styles.css";
+import Form from "./home/index.js";
+
+function App() {
+  return (
+    <CardSong />
+  );
+}
+export default function App() {
+  return <Form />;
+}
+
+export default App;
+
---
+++ homework-module-2/src/home/index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function selectForm() {
+  return (
+    <form>
+      <input type="text" id="searchBar" placeholder="Select"></input>
+      <input type="submit" id="searchBtn" value="Select"></input>
+      <div>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/9/9f/Bohemian_Rhapsody.png"
+          alt="pic"
+        />
+      </div>
+    </form>
+  );
+}
