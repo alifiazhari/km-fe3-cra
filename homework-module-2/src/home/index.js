0e8783d89d8331b79879471db99f73cac5f93bf0
Alif <alifia71001@gmail.com>
2022-03-30 14:23:26 -0700

    i tried my best really

--- homework-module-2/src/home/index.js
+++ homework-module-2/src/home/index.js
@@ -3,15 +3,19 @@ import "./src/component/img";
 
 export default function selectForm() {
   return (
-    <form>
-      <input type="text" id="searchBar" placeholder="Select"></input>
-      <input type="submit" id="searchBtn" value="Select"></input>
-      <div>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/en/9/9f/Bohemian_Rhapsody.png"
-          alt="pic"
-        />
-      </div>
-    </form>
+    <><head>
+      <title>GENERASI GIGIH 2.0 HOMEWORK</title>
+      <meta charset="UTF-8" />
+      <link rel stylesheet />" href="src/styles.css" />
+      <button onclicks="myFunction" />()>Submit<button>
+      </></head><form>
+        <input type="text" id="searchBar" placeholder="Select"></input>
+        <input type="submit" id="searchBtn" value="Select"></input>
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/9/9f/Bohemian_Rhapsody.png"
+            alt="pic" />
+        </div>
+      </form><a href="https://developer.spotify.com/documentation/general/guides/authorization/">Spotify Auth API</a></></>
   );
 }
