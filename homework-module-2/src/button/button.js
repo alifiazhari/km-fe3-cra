495f3e47940947af184b434ada31eabe591e1ca2
Alif <alifia71001@gmail.com>
2022-04-04 01:22:34 -0700

    try again pls

---
+++ homework-module-2/src/button/button.js
@@ -0,0 +1,16 @@
+import  './button.css';
+function button () {
+    return (
+        <><button class="button-7" role="button">Button 7</button><><div>
+            let btn = document.createElement("button");
+            btn.innerHTML = "Select";
+            document.body.appendChild(btn);
+        </div>
+
+            <div>
+                if let btn = document.createElement("button");
+                btn.innerHTML = "Deselect";
+                document.body.appendChild(btn);
+            </div></></>
+    );
+}
--- homework-module-2/src/home/index.js
+++ homework-module-2/src/home/index.js
@@ -1,6 +1,33 @@
 import "./styles.css";
 import "./src/component/img";
 
+function () {
+  var files = [
+      "sample1.mp3",
+      "sample2.mp3",
+      "sample3.mp3"
+  ];
+  var i = 0;
+  var music_player = document.querySelector("#music_list audio");
+
+  function next() {
+     
+      if (i === files.length - 1) {
+          i = 0;
+      } else {
+          i++;
+      }
+      music_player.src = files[i];
+  }
+  if (music_player === null) {
+      throw "Playlist Player does not exists ...";
+  } else {
+      music_player.src = files[i];
+      music_player.addEventListener('ended', next, false)
+  }
+
+})();
+);
 export default function selectForm() {
   return (
     <><head>
@@ -16,6 +43,9 @@ export default function selectForm() {
             src="https://upload.wikimedia.org/wikipedia/en/9/9f/Bohemian_Rhapsody.png"
             alt="pic" />
         </div>
-      </form><a href="https://developer.spotify.com/documentation/general/guides/authorization/">Spotify Auth API</a></></>
-  );
+      </form>
+      <a href="https://developer.spotify.com/documentation/general/guides/authorization/">Spotify Auth API</a></></>     
+      <div id="music_list">
+      <audio controls autoplay></audio>
+      </div>
 }
